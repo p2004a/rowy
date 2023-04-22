95a12fd96ce2e65e52f0857035b537cef48be3ac
Marek Rusinowski <marekrusinowski@gmail.com>
2023-04-22 22:21:57 +0200

    Reload the page when the set of roles after toke update is different

--- src/sources/RefreshAuth.tsx
+++ src/sources/RefreshAuth.tsx
@@ -9,7 +9,22 @@ import {
 } from "@src/atoms/projectScope";
 
 /**
+ * Returns true if two sets are equal.
+ */
+function setEqual<T>(a: Set<T>, b: Set<T>): boolean {
+  if (a.size !== b.size) return false;
+  for (const v of a) {
+    if (!b.has(v)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
  * When rendered, sets up listeners to refresh auth token when needed.
+ *
+ * When auth token is refreshed and it contains different roles, the page is reloaded.
  */
 const RefreshAuth = memo(function RefreshAuth() {
   const [userSettings] = useAtom(userSettingsAtom, projectScope);
@@ -19,9 +34,17 @@ const RefreshAuth = memo(function RefreshAuth() {
     if (currentUser && userSettings && userSettings.refreshTime) {
       const refreshTime = userSettings.refreshTime;
       getIdTokenResult(currentUser).then((tokenResult) => {
+        const oldRoles = new Set((tokenResult.claims.roles as string[]) ?? []);
         const tokenIssueTime = new Date(tokenResult.issuedAtTime).getTime();
         if (refreshTime > tokenIssueTime) {
-          currentUser.getIdToken(true);
+          getIdTokenResult(currentUser, true).then((newTokenResult) => {
+            const newRoles = new Set(
+              (newTokenResult.claims.roles as string[]) ?? []
+            );
+            if (!setEqual(oldRoles, newRoles)) {
+              window.location.reload();
+            }
+          });
         }
       });
     }
